1feec085e8172b63af991228708939f8322e294b
TMuyi <timothymuyiwa@gmail.com>
2022-03-01 20:57:18 +0100

    Update jumia.js
    fixed fixture aliasing

--- cypress/integration/jumia.js
+++ cypress/integration/jumia.js
@@ -6,46 +6,49 @@ import SearchPage from "../support/pageObjects/SearchPage";
 import LoginPage from "../support/pageObjects/LoginPage";
 
 describe("rstestv8", () => {
-  let data;
-  before(() => {
+  beforeEach(() => {
     cy.visit(Cypress.env("url")); //visit
 
-    cy.fixture("users").then((fdata) => {
-      data = fdata;
-    });
+    cy.fixture("users").as("data");
   });
 
   it("Login test", () => {
     const defaultPage = new DefaultPage();
     const loginPage = new LoginPage();
-
-    defaultPage.getAccount().should("be.visible").click();
-    defaultPage.getLogin().should("be.visible").click();
-    cy.title().should("eq", "Login Page"); //move?
-    loginPage.getEmail().type(data.email, { delay: 50 });
-    loginPage.getPassword().type(data.password, { delay: 50 });
-    loginPage.getPasswordVis().click();
-    loginPage.clickFinalLogin().click();
+    cy.get("@data").then((data) => {
+      defaultPage.getAccount().should("be.visible").click();
+      defaultPage.getLogin().should("be.visible").click();
+      cy.title().should("eq", "Login Page"); //move?
+      loginPage.getEmail().type(data.email, { delay: 50 });
+      loginPage.getPassword().type(data.password, { delay: 50 });
+      loginPage.getPasswordVis().click();
+      loginPage.clickFinalLogin().click();
+    });
   }); //it
 
-  it.only("Signup test", () => {
+  it("Signup test", () => {
     const loginPage = new LoginPage();
     const defaultPage = new DefaultPage();
 
-    defaultPage.getAccount().should("be.visible").click();
-    defaultPage.getLogin().should("be.visible").click();
-    cy.title().should("eq", "Login Page"); //move?
-    loginPage.getRegistration().click();
-    loginPage.getFirstName().type(data.firstName, { delay: 50 });
-    loginPage.getLastName().type(data.lastName, { delay: 50 });
-    loginPage.getRegEmail().type(data.email, { delay: 50 });
-    loginPage.getRegPassword().type(data.password, { delay: 50 });
-    loginPage.getPhoneNo().type(data.phoneNumber, { delay: 50 });
+    cy.get("@data").then((data) => {
+      defaultPage.getAccount().should("be.visible").click();
+      defaultPage.getLogin().should("be.visible").click();
+      cy.title().should("eq", "Login Page"); //move?
+      loginPage.getRegistration().click();
+      loginPage.getFirstName().type(data.firstName, { delay: 50 });
+      loginPage.getLastName().type(data.lastName, { delay: 50 });
+      loginPage.getRegEmail().type(data.email, { delay: 50 });
+      loginPage.getRegPassword().type(data.password, { delay: 50 });
+      loginPage.getPhoneNo().type(data.phoneNumber, { delay: 50 });
 
-    loginPage.getCheckBoxNewsie().uncheck({ force: true });
-    loginPage.getCreateAccount().click(); //negative asssertion here for CheckBoxTerms()
-    loginPage.getCheckBoxTerms().check({ force: true });
-    // loginPage.getCreateAccount().click(); don't want to actually sign up
-    //
+      loginPage.getCheckBoxNewsie().uncheck({ force: true });
+      loginPage.getCreateAccount().click(); //negative asssertion here for CheckBoxTerms()
+      loginPage.getCheckBoxTerms().check({ force: true });
+      // loginPage.getCreateAccount().click(); don't want to actually sign up
+    });
+  }); //it
+
+  it("search for product and add to cart", () => {
+    const defaultPage = new DefaultPage();
   }); //it
 }); //describe
